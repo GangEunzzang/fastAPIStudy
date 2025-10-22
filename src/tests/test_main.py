58f5fb50997869f4fb32663a396c8b7069d29c68
이강은 <rkddms123456@gmail.com>
2025-10-22 23:56:40 +0900

    feat: add pytest configuration and update tests for health check and todos

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,22 +1,23 @@
-from fastapi.testclient import TestClient
+from database.orm import Todo
 
-from main import app
-
-client = TestClient(app)
-
-
-def test_health_check_handler():
+def test_health_check_handler(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
 
 
-def test_get_todos():
+def test_get_todos(client, mocker):
+    mocker.patch("main.get_todos", return_value=[
+        Todo(id=1, contents="FastAPI Section 0", is_done=True),
+        Todo(id=2, contents="FastAPI Section 1", is_done=False),
+    ])
+
     response = client.get("/todos")
+
     assert response.status_code == 200
     assert isinstance(response.json(), list)
-    assert response.json() == {
-        "todos": [
-            {"id": 1, "contents": "FastAPI Section 0", "is_done": True},
-        ]
-    }
+
+    assert response.json() == [
+        {"id": 1, "contents": "FastAPI Section 0", "is_done": True},
+        {"id": 2, "contents": "FastAPI Section 1", "is_done": False},
+    ]
